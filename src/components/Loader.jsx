6d1ac72c6ff533fcbcd8f60a541bc296c3431529
Erick Code <3rickcedeno@gmail.com>
2023-05-28 20:02:56 -0500

    Add:Nnew loader and favicon

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,26 +2,106 @@ import React from 'react'
 import "./styles/Loader.css"
 const Loader = () => {
   return (
-    <div className="wrapper  bg-no-repeat min-h-screen bg-cover">
+    <section className=''>
+         <div className='parrafo'>
+         <p className=''>Weather App</p>
+         </div>
+       
+
+      <div className="wrapper">
+    <div className="sun"></div>
     <div className="cloud">
-      <div className="cloud_left"></div>
-      <div className="cloud_right"></div>
+      <div className="cloud1">
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div className="cloud1 c_shadow">
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
     </div>
+ 
+    <div className="cloud_s">
+      <div className="cloud1">
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div className="cloud1 c_shadow">
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+    
+    <div className="cloud_vs">
+      <div className="cloud1">
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div className="cloud1 c_shadow">
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+    <div className="haze"></div>
+    <div className="haze_stripe"></div>
+    <div className="haze_stripe"></div>
+    <div className="haze_stripe"></div>
+    <div className="thunder"></div>
     <div className="rain">
-      <div className="drop"></div>
-      <div className="drop"></div>
-      <div className="drop"></div>
-      <div className="drop"></div>
-      <div className="drop"></div>
+       <ul>
+         <li></li>
+         <li></li>
+         <li></li>
+      </ul>
     </div>
-    <div className="surface">
-      <div className="hit"></div>
-      <div className="hit"></div>
-      <div className="hit"></div>
-      <div className="hit"></div>
-      <div className="hit"></div>
+    <div className="sleet">
+       <ul>
+         <li></li>
+         <li></li>
+         <li></li>
+      </ul>
+    </div>
+    <div className="text">
+      <ul>
+        <li>Soleado</li>
+        <li>Parcialmente Soleado</li>
+        <li>Parcialmente Nublado</li>
+        <li>Mayormente Nublado</li>
+        <li>Nublado</li>
+        <li>Brumoso</li>
+        <li>Tormenta</li>
+        <li>Lluvia</li>
+        <li>Nieve y Lluvia</li>
+      </ul>
     </div>
   </div>
+
+    </section>
+
   )
 }
 
